31bf6bce13489837e28610d6b4d265c542ac0e1c
KirillBaranov <kirillBaranovJob@yandex.ru>
2025-11-19 14:47:08 +0300

    refactor(devlink): remove remaining legacy devlink modules
    Remove old devlink plan, scan, status, switch, types, update, and watch
    modules that have been replaced by the new architecture.

--- packages/core/src/devlink/watch/index.ts
+++ packages/core/src/devlink/watch/index.ts
@@ -1,9 +1,1 @@
-// Экспорт новой версии как единственной
-export { DevLinkWatcher, watchDevLink } from "./watch";
-export { detectMode } from "./mode-detector";
-export { detectBuildCommands, detectWatchPaths, shouldIgnorePath, getWatchPatterns } from "./build-detector";
-export { buildReverseDependencyMap, getDirectConsumers, filterProviders, filterConsumers } from "./dependency-resolver";
-export * from "./types";
-export * from "./events";
-export type { AllDevLinkEvents } from "./events";
-
+export * from '@devlink/application/devlink/legacy/watch';
